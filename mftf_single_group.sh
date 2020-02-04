520a7fa36e576d602b1ad1fdc6e95a86ee72c998
Tom Reece <treece@adobe.com>
2020-02-04 09:43:25 -0600

    Add mftf_single_group.sh

---
+++ mftf_single_group.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e ${GROUP_TO_RUN}
+
+# Setup
+docker-compose down -v > /dev/null 2>&1
+rm -rf html > /dev/null 2>&1
+tar -xf install.tar
+echo -e "\e[36mStarting docker containers\e[0m"
+docker-compose up -d
+docker ps -a
+sleep 5s;
+bash restoredb.sh
+
+# Run a specific group
+docker-compose exec -T -e group=${GROUP_TO_RUN} fpm bash /tmp/files/perform_tests.sh
+
+echo -e "\e[95mDone\e[0m"
