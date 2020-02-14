793d548e8b064af671b7340a31f96ffc94796e12
Tom Reece <treece@adobe.com>
2020-02-14 16:51:31 -0600

    Add comment explaining CURRENT_TEST

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
 $test = $_GET['test'] ?? "NO_TEST_SPECIFIED";
+// Since this file is located at pub/test.php we need to create the CURRENT_TEST file one level up in the Magento root
 file_put_contents('../CURRENT_TEST', $test);
 echo 'SET CURRENT TEST TO ' . $test;
