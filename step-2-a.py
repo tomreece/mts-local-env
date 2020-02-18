dd4407d71cf228fdf4c30f33493acabf2a81b9e8
Tom Reece <treece@adobe.com>
2020-02-18 16:33:39 -0600

    Try to trigger two jobs:

--- step-2-a.py
+++ step-2-a.py
@@ -2,4 +2,34 @@ import boto3
 
 client = boto3.client('codebuild')
 
-client.start_build(projectName='code-coverage-two')
+client.start_build(
+    projectName='code-coverage-two',
+    environmentVariablesOverride=[
+        {
+            'name': 'PREV_BUILD_ID',
+            'value': '7e375873-1fbe-48d6-b555-80b90b768423',
+            'type': 'PLAINTEXT'
+        },
+        {
+            'name': 'GROUP_NUMBER',
+            'value': '11',
+            'type': 'PLAINTEXT'
+        }
+    ]
+)
+
+client.start_build(
+    projectName='code-coverage-two',
+    environmentVariablesOverride=[
+        {
+            'name': 'PREV_BUILD_ID',
+            'value': '7e375873-1fbe-48d6-b555-80b90b768423',
+            'type': 'PLAINTEXT'
+        },
+        {
+            'name': 'GROUP_NUMBER',
+            'value': '25',
+            'type': 'PLAINTEXT'
+        }
+    ]
+)
