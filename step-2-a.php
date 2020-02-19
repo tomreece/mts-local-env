f50572a384f3463ebb1c760c2f00cc48ad2623ba
Tom Reece <treece@adobe.com>
2020-02-19 16:53:48 -0600

    Code cleanup

--- step-2-a.php
+++ step-2-a.php
@@ -4,60 +4,82 @@ require 'vendor/autoload.php';
 
 use Aws\CodeBuild\CodeBuildClient;
 
-$PREV_BUILD_ID = '7e375873-1fbe-48d6-b555-80b90b768423';
-$NUM_GROUPS = 2;
+class GroupsRunner {
+    private $prevBuildId;
+    private $numGroups;
+    private $codebuild;
+    private $builds;
 
-$codebuild = new CodeBuildClient([
-    'version' => 'latest',
-    'region' => 'us-east-1'
-]);
+    function __construct($prevBuildId, $numGroups)
+    {
+        $this->prevBuildId = $prevBuildId;
+        $this->numGroups = $numGroups;
+        $this->codebuild = new CodeBuildClient([
+            'version' => 'latest',
+            'region' => 'us-east-1'
+        ]);
+    }
 
-$builds = [];
+    public function main()
+    {
+        $this->startAllGroups();
+        $this->pollBuilds();
 
-for ($i = 1; $i <= $NUM_GROUPS; $i++) {
-    $builds[] = $codebuild->startBuild([
-        'projectName' => 'code-coverage-two',
-        'environmentVariablesOverride' => [
-            [
-                'name' => 'PREV_BUILD_ID',
-                'value' => $PREV_BUILD_ID
-            ],
-            [
-                'name' => 'GROUP_NUMBER',
-                'value' => "$i"
-            ]
-        ]
-    ]);
-}
+        print('All done!');
+    }
 
-$numTotal = count($builds);
-
-while(true) {
-    print("Polling...");
-
-    $statuses = $codebuild->batchGetBuilds([
-        'ids' => array_map(function($build) { return $build['build']['id']; }, $builds)
-    ]);
-
-    $allDone = true;
-    $numInProgress = 0;
-
-    foreach ($statuses['builds'] as $build) {
-        $status = $build['buildStatus'];
-        if ($status === 'IN_PROGRESS') {
-            $allDone = false;
-            $numInProgress += 1;
+    private function startAllGroups()
+    {
+        for ($i = 1; $i <= $this->numGroups; $i++) {
+            $this->builds[] = $this->codebuild->startBuild([
+                'projectName' => 'code-coverage-two',
+                'environmentVariablesOverride' => [
+                    [
+                        'name' => 'PREV_BUILD_ID',
+                        'value' => $this->prevBuildId
+                    ],
+                    [
+                        'name' => 'GROUP_NUMBER',
+                        'value' => "$i"
+                    ]
+                ]
+            ]);
         }
     }
 
-    $numFinished = $numTotal - $numInProgress;
-    print("$numFinished out of $numTotal jobs are finished.\n");
+    private function pollBuilds() {
+        $numTotal = count($this->builds);
 
-    if ($allDone) {
-        break;
+        while(true) {
+            print('Polling...');
+
+            $statuses = $this->codebuild->batchGetBuilds([
+                'ids' => array_map(function($build) { return $build['build']['id']; }, $this->builds)
+            ]);
+
+            $allDone = true;
+            $numInProgress = 0;
+
+            foreach ($statuses['builds'] as $build) {
+                $status = $build['buildStatus'];
+                if ($status === 'IN_PROGRESS') {
+                    $allDone = false;
+                    $numInProgress += 1;
+                }
+            }
+
+            $numFinished = $numTotal - $numInProgress;
+            print("$numFinished out of $numTotal jobs are finished.\n");
+
+            if ($allDone) {
+                break;
+            }
+
+            sleep(30);
+        }
     }
-
-    sleep(30);
 }
 
-print("All Done!\n");
+// todo -- pass these in as arguments to the script
+$groupRunner = new GroupsRunner('7e375873-1fbe-48d6-b555-80b90b768423', 2);
+$groupRunner->main();
